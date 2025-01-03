4ada7368e124597b3b23508474261864651c437f
Trivendra_Singh <trivendrasingh0711@gmail.com>
2025-01-03 09:38:56 +0530

    sol

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,6 +1,9 @@
+const e = require('express');
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
-
+const zod = require('zod');
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 
 /**
  * Generates a JWT for a given username and password.
@@ -14,7 +17,15 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null
+    }
+    const signature = jwt.sign({
+        username
+    },jwtPassword)
+    return signature
 }
 
 /**
@@ -26,9 +37,14 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+let ans = true;
+try{
+    jwt.verify(token, jwtPassword);
+} catch (e){
+    ans = false;
 }
-
+return ans;
+}
 /**
  * Decodes a JWT to reveal its payload without verifying its authenticity.
  *
@@ -37,7 +53,12 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+const decoded = jwt.decode(token);
+if(decoded){
+    return true;
+} else {
+    return false;
+}
 }
 
 
